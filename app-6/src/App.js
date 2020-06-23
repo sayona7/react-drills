d2de9f9b38e7b7af6cd54a98474fc2edaccb4270
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-06-22 19:47:36 -0500

    solved problem 6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from "./Components/Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      array: [],
+      input: ""
+    };
+
+    this.buttonHandler = this.buttonHandler.bind(this);
+  }
+
+inputHandler(value) {
+  this.setState({input: value})
 }
+
+buttonHandler() {
+  this.setState({
+    array: [...this.state.array, this.state.input],
+    input: ""
+  })
+}
+
+  render() {
+    let array = this.state.array.map((element, index) => {
+      return <Todo key={index} task={element} />;
+    });
+    
+    return (
+      <div className="div">
+        <h3>To-do list:</h3>
+        <input placeholder="Add your task!"
+        value={this.state.input}
+        onChange={e => this.inputHandler(e.target.value)} />
+        <button onClick={this.buttonHandler} id="btn">Add</button>
+        <Todo />
+
+        {array}
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
