6bb58259e323ca360f51b628aa4e6e3071a0ccf8
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-16 13:19:14 -0500

    added NewTAsk

--- app-7/src/NewTask.js
+++ app-7/src/NewTask.js
@@ -3,12 +3,34 @@ import React, { Component } from 'react';
 class NewTask extends Component {
     constructor() {
         super();
-        this.state = {  }
+        this.state = { 
+            input: ""
+         };
+
+
+         this.handleAdd = this.handleAdd.bind(this);
     }
+
+    handleInputChange(val) {
+        this.setState({input: val});
+    }
+
+    handleAdd() {
+        this.props.add(this.state.input);
+        this.setState({input: ""});
+    }
+
+
+
     render() { 
         return ( 
             <div>
-                
+                <input
+                value={this.state.input}
+                placeholder="Enter new task"
+                onChange={e => this.handleInputChange(e.target.value)} />
+
+                <button onClick={this.handleAdd}>Add</button>
             </div>
          );
     }
