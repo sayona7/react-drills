83db5c960110ecb0fe0774c7decb200abf452f30
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-16 12:46:56 -0500

    setting up files in problem 7

---
+++ app-7/src/NewTask.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NewTask extends Component {
+    constructor() {
+        super();
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default NewTask;
